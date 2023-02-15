40013741ef087d4faf6418ff24746448d2b52d13
Надюшка Рыбка <kitti-46@ya.ru>
2023-02-16 00:07:18 +0300

    added task3 logg

--- task3HOME.java
+++ task3HOME.java
@@ -1,9 +1,19 @@
 
 // 3. Реализовать простой калькулятор
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.XMLFormatter;
 
 public class task3HOME {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Logger logger = Logger.getLogger(task3HOME.class.getName());
+        FileHandler fh = new FileHandler("log1.txt");
+        logger.addHandler(fh);
+        XMLFormatter xml = new XMLFormatter();
+        fh.setFormatter(xml);
+        logger.info("Простой калькулятор");
         Scanner iScanner = new Scanner(System.in);
         System.out.printf("Введите первое число: ");
         float a = iScanner.nextFloat();
